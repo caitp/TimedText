61b1db2780e623be78efa77a8c9dee528ae1efaf
Caitlin Potter <snowball@defpixel.com>
2013-06-25 01:43:52 -0400

    Updated license in many files, and removed Makefile
    Makefile is generated by the configure script as a convenience,
    but should not be included in the repository.

--- include/TimedText/Cue.h
+++ include/TimedText/Cue.h
@@ -1,3 +1,30 @@
+//
+// Copyright (c) 2014 Caitlin Potter and Contributors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//  * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
 #ifndef __TimedText_Cue__
 #define __TimedText_Cue__
 
@@ -11,10 +38,26 @@ class Cue
 public:
   ~Cue();
 
-  String id;
-  Timestamp startTime;
-  Timestamp endTime;
-  bool pauseOnExit;
+  String id() const {
+  	return _id;
+  }
+
+  // TTML: begin
+  Timestamp startTime() const {
+  	return _startTime;
+  }
+  Timestamp endTime() const {
+  	return _endTime;
+  }
+  bool pauseOnExit() const {
+  	return _pauseOnExit;
+  }
+
+protected:
+  String _id;
+  Timestamp _startTime;
+  Timestamp _endTime;
+  bool _pauseOnExit;
 };
 
 }
